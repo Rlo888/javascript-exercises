a5d8a178dd76104346f2bc5f7eeb067d178c4aa7
Ryan <asdfasdf>
2022-10-25 20:59:09 +0800

    passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function (str, number) {
+  if (number < 0) {
+    return "ERROR";
+  }
+  let newStr = str.repeat(number);
 
+  return newStr;
 };
 
 // Do not edit below this line
