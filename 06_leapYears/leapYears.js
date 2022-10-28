3e32ff1068e95d1183202ae1ca72b87f9230b290
Ryan <asdfasdf>
2022-10-28 22:22:00 +0800

    passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function (x) {
+  let year = 0;
+  if (x % 400 == 0) {
+    return true;
+  } else if (x % 4 == 0 && x % 100 !== 0) {
+    return true;
+  } else if (x) return false;
 };
 
 // Do not edit below this line
